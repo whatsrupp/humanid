809a06378a7ceb78198d3ea305fac51d860665eb
whatsrupp <nickrupp95@gmail.com>
2019-02-26 01:13:06 +0000

    addfonts so native base works

--- App.js
+++ App.js
@@ -35,6 +35,8 @@ export default class App extends React.Component {
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
+        'Roboto': require('native-base/Fonts/Roboto.ttf'),
+       'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
         ...Icon.Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
