f5d981fe980da37ef15ad1fcf7f8171f5ed747a5
whatsrupp <nickrupp95@gmail.com>
2019-02-26 23:35:36 +0000

    hook up formik to basic qr component

--- screens/Log/LogScreen.js
+++ screens/Log/LogScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 
-import { Picker, Container, ListItem, Radio, Header, Content, Title,Left, Right, Body, Form, Item, Input, Icon, Label, DatePicker, Separator, Text } from 'native-base';
+import { Picker, Button, Container, ListItem, Radio, Header, Content, Title,Left, Right, Body, Form, Item, Input, Icon, Label, DatePicker, Separator, Text } from 'native-base';
+import { Formik } from 'formik';
 
 
 import { MonoText } from '../../components/StyledText';
@@ -36,78 +37,24 @@ export default class LogScreen extends React.Component {
           <Body><Title>Log Data</Title></Body>
         </Header>
         <Content>
-        <Form>
-            <Item>
-              <Icon active type='FontAwesome' name='qrcode' />
-              <Input placeholder='Scan QR Code'/>
-            </Item>
-            <Item>
-              <Icon active type='FontAwesome' name='map-marker' />
-              <Input placeholder='Place of Disaster'/>
-            </Item>
-            <Item>
-              <Icon active type='FontAwesome' name='calendar' />
-              <Input placeholder='Date of Disaster'/>
-            </Item>
-            <Item>
-            <Icon active type='FontAwesome' name='transgender' />
-            <Right>
-
-            <Picker
-            placeholder="Gender"
-              mode="dropdown"
-              iosHeader="Select Gender"
-              iosIcon={<Icon name="arrow-down" />}
-              selectedValue={this.state.selected}
-              onValueChange={this.onValueChange.bind(this)}
-
-            >
-              <Picker.Item label="Male" value="key0" />
-              <Picker.Item label="Female" value="key1" />
-              <Picker.Item label="Unknown" value="key2" />
-            </Picker>
-            </Right>
-      
-            </Item>
-
-            <Separator bordered>
-            <Text>Forensic Evidence</Text>
-          </Separator>
-
-            <ListItem>
-            <Left>
-              <Text>Fingerprint</Text>
-            </Left>
-            <Right>
-              <Radio selected={true} />
-            </Right>
-          </ListItem>
-
-          <ListItem>
-            <Left>
-              <Text>Skin Sample</Text>
-            </Left>
-            <Right>
-              <Radio selected={true} />
-            </Right>
-          </ListItem>
-
-          <ListItem>
-            <Left>
-              <Text>Hair</Text>
-            </Left>
-            <Right>
-              <Radio selected={this.state.hair}/>
-            </Right>
-          </ListItem>
-          <Separator bordered>
-            <Text>Physical Evidence</Text>
-          </Separator>
-          <Item>
-              <Input placeholder='Add physical evidence'/>
-          </Item>
-
-          </Form>
+        <Formik
+          initialValues={{}}
+          onSubmit={(values, actions) => {
+            console.log(values)
+          }}
+          render={props =>{
+            console.log(props.values)
+            return(
+          <Form>
+              <Item>
+                <Icon active type='FontAwesome' name='qrcode' />
+                <Input placeholder='Scan QR Code' onChangeText={props.handleChange('qrCode')} onBlur={props.handleBlur('qrCode')} value={props.values.name} name="qrCode"/>
+              </Item>
+              <Button full success onPress={props.handleSubmit}><Text>Submit</Text></Button>
+            </Form>
+          )}}
+        >
+          </Formik>
         </Content>
       </Container>
       
