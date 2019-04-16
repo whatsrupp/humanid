bb430832f4c183ff402011f56ccc510abedda50e
whatsrupp <nickrupp95@gmail.com>
2019-04-16 22:27:34 +0100

    reformat retrieve screen

--- screens/Retrieve/RetrieveScreen.js
+++ screens/Retrieve/RetrieveScreen.js
@@ -1,73 +1,92 @@
 import React from 'react';
+import {
+  Card,
+  Fab,
+  Icon,
+  Container,
+  Header,
+  Content,
+  Title,
+  Body,
+  Text,
+  CardItem,
+  Thumbnail
+} from 'native-base';
+import { Modal, ScrollView } from 'react-native';
 
-import {  Card, Fab, Icon, Container, Header, Content, Title, Body, Text, CardItem, Thumbnail } from 'native-base';
-import {  Modal, ScrollView } from 'react-native';
-import QRScanner from '../Log/QrScanner'
-import {getEntryByQrCode} from './requests/getEntry'
+import QRScanner from '../Log/QrScanner';
+import { getEntryByQrCode } from './requests/getEntry';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
-    title: 'Retrieve Screen',
+    title: 'Retrieve Screen'
   };
 
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       data: null,
       isScanning: false,
       qrCode: null
-    }
+    };
   }
 
-  handleQRSubmit = async (qrCode)=>{
-    await this.setState({qrCode,isScanning: false, data})
-    const data = await getEntryByQrCode(qrCode)
-    await this.setState({data})
+  handleQRSubmit = async qrCode => {
+    await this.setState({ qrCode, isScanning: false, data });
+    const data = await getEntryByQrCode(qrCode);
+    await this.setState({ data });
+  };
 
-  }
+  renderThumbnails = urls => {
+    const thumbnailSize = 50;
 
-  renderThumbnails = (urls)=>{
-    const thumbnailSize = 50
+    return (
+      <ScrollView horizontal showsHorizontalScrollIndicator>
+        {urls.map((url, index) => {
+          return (
+            <Thumbnail
+              key={index}
+              source={{ uri: url }}
+              style={{
+                marginLeft: index == 0 ? 0 : 10,
+                marginRight: 10,
+                height: thumbnailSize,
+                width: thumbnailSize,
+                borderRadius: thumbnailSize / 2
+              }}
+            />
+          );
+        })}
+      </ScrollView>
+    );
+  };
 
-    return(
-        <ScrollView
-        horizontal
-      showsHorizontalScrollIndicator>
-              {urls.map((url, index)=>{
-
-       return( <Thumbnail key={index} source={{uri: url}} style={{marginLeft: index == 0 ? 0 : 10, marginRight: 10, height: thumbnailSize, width: thumbnailSize, borderRadius: thumbnailSize/2}}/>)
-      })}
-
-        </ScrollView>
-      )
-  
-  }
-
-  hasData = ()=>{
-    return !!this.state.data
-  }
+  hasData = () => {
+    return !!this.state.data;
+  };
 
   renderQR = () => {
-    if(!this.state.isScanning) return null 
-    return (<Modal
-      animationType="slide"
-      >
-      <QRScanner handleQRSubmit={this.handleQRSubmit} />
-      <Fab
-            style={{backgroundColor: '#FF6347' }}
-            position="bottomRight"
-            onPress={() => this.setState({ isScanning: !this.state.isScanning })}>
-            <Icon active type='FontAwesome' name='qrcode' />
-          </Fab>
-    </Modal>)
-  }
+    if (!this.state.isScanning) return null;
+    return (
+      <Modal animationType="slide">
+        <QRScanner handleQRSubmit={this.handleQRSubmit} />
+        <Fab
+          style={{ backgroundColor: '#FF6347' }}
+          position="bottomRight"
+          onPress={() => this.setState({ isScanning: !this.state.isScanning })}
+        >
+          <Icon active type="FontAwesome" name="qrcode" />
+        </Fab>
+      </Modal>
+    );
+  };
 
-  renderDataDisplay = ()=>{
-    const {data} = this.state
-    
-    if(!data){
-      return(
+  renderDataDisplay = () => {
+    const { data } = this.state;
+
+    if (!data) {
+      return (
         <Card bordered>
           <CardItem header>
             <Text>No Data Yet!</Text>
@@ -76,13 +95,23 @@ export default class HomeScreen extends React.Component {
             <Text>Please Scan a QR code to retrieve data</Text>
           </CardItem>
         </Card>
-      )
-      
+      );
     }
-    const {physicalEvidenceEntries, skin, qrCode, photoUrls, latitude, longitude, hair, gender, fingerprint, dateOfEntry} = data
-    
-    return(
-      <Card >
+    const {
+      physicalEvidenceEntries,
+      skin,
+      qrCode,
+      photoUrls,
+      latitude,
+      longitude,
+      hair,
+      gender,
+      fingerprint,
+      dateOfEntry
+    } = data;
+
+    return (
+      <Card>
         <CardItem header bordered>
           <Text>General Information</Text>
         </CardItem>
@@ -91,7 +120,7 @@ export default class HomeScreen extends React.Component {
         </CardItem>
         <CardItem bordered>
           <Text>QR Code: {qrCode}</Text>
-        </CardItem >
+        </CardItem>
         <CardItem bordered>
           <Text>Gender: {gender.toUpperCase()}</Text>
         </CardItem>
@@ -112,56 +141,48 @@ export default class HomeScreen extends React.Component {
           <Text>Hair: {hair === 1 ? 'True' : 'False'}</Text>
         </CardItem>
 
-        <CardItem bordered >
+        <CardItem bordered>
           <Text>Skin Sample: {skin === 1 ? 'True' : 'False'}</Text>
         </CardItem>
 
         <CardItem bordered header>
           <Text>Photos</Text>
         </CardItem>
-        <CardItem bordered>
-        {this.renderThumbnails(photoUrls)}
-        </CardItem>
+        <CardItem bordered>{this.renderThumbnails(photoUrls)}</CardItem>
 
         <CardItem bordered header>
           <Text>Physical Evidence</Text>
         </CardItem>
 
         {physicalEvidenceEntries.map((entry, index) => {
-          return(
+          return (
             <CardItem bordered key={index}>
               <Text>{entry}</Text>
-            </CardItem>      
-          )
+            </CardItem>
+          );
         })}
-
-
       </Card>
-    )
-  }
+    );
+  };
 
   render = () => {
     return (
       <Container>
-
         {this.renderQR()}
         <Header>
-          <Body><Title>Retrieve Data</Title></Body>
+          <Body>
+            <Title>Retrieve Data</Title>
+          </Body>
         </Header>
-        <Content style={{padding: 20}}>
-        
-          {this.renderDataDisplay()}
-
-        </Content>
-          <Fab
-            style={{ backgroundColor: '#5067FF' }}
-            position="bottomRight"
-            onPress={() => this.setState({ isScanning: !this.state.isScanning })}>
-            <Icon active type='FontAwesome' name='qrcode' />
-          </Fab>
+        <Content style={{ padding: 20 }}>{this.renderDataDisplay()}</Content>
+        <Fab
+          style={{ backgroundColor: '#5067FF' }}
+          position="bottomRight"
+          onPress={() => this.setState({ isScanning: !this.state.isScanning })}
+        >
+          <Icon active type="FontAwesome" name="qrcode" />
+        </Fab>
       </Container>
     );
-  }
-
-  
+  };
 }
--- screens/Retrieve/requests/getEntry.js
+++ screens/Retrieve/requests/getEntry.js
@@ -3,31 +3,35 @@ import { SQLite } from 'expo';
 const db = SQLite.openDatabase('humanId');
 
 const executeSql = async (sql, params = []) => {
-    return new Promise((resolve, reject) => db.transaction(tx => {
-        tx.executeSql(
-            sql, 
-            params,
-            (_, result) => resolve(result),
-            (_, error) => reject(error)
-        )
-    }))
-}
+  return new Promise((resolve, reject) =>
+    db.transaction(tx => {
+      tx.executeSql(
+        sql,
+        params,
+        (_, result) => resolve(result),
+        (_, error) => reject(error)
+      );
+    })
+  );
+};
 
-const deserializeStringArray = (list) => {
-    const desrializedArray = list.split(';')
-    return desrializedArray
-}
+const deserializeStringArray = list => {
+  const desrializedArray = list.split(';');
+  return desrializedArray;
+};
 
-export const getEntryByQrCode = async (qrCode) =>{
-    const sql =`SELECT * FROM entries
+export const getEntryByQrCode = async qrCode => {
+  const sql = `SELECT * FROM entries
     WHERE qrCode = ?
     ORDER BY dateOfEntry DESC
-    LIMIT 1`
+    LIMIT 1`;
 
-    const parameters = [qrCode]
-    const data = await executeSql(sql, parameters)
-    const entry = data.rows._array[0]
-    entry.physicalEvidenceEntries = deserializeStringArray(entry.physicalEvidence)
-    entry.photoUrls = deserializeStringArray(entry.photoUrls)    
-    return entry
-}
+  const parameters = [qrCode];
+  const data = await executeSql(sql, parameters);
+  const entry = data.rows._array[0];
+  entry.physicalEvidenceEntries = deserializeStringArray(
+    entry.physicalEvidence
+  );
+  entry.photoUrls = deserializeStringArray(entry.photoUrls);
+  return entry;
+};
